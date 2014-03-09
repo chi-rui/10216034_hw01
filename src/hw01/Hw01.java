d499dfaa77596513d5e4693c8567b46179b368d7
chi-rui <xic768210@yahoo.com.tw>
2014-03-09 15:27:01 +0800

    把每個方程式用class建立起來
    搭好繼承關係&填寫裡面的東西

--- src/hw01/Hw01.java
+++ src/hw01/Hw01.java
@@ -4,26 +4,70 @@ public abstract class Hw01 {
 	abstract double f(double x); 	
 }
 
-class X2{
+class X2 extends Hw01{
+	double a;
+	X2(double x){
+		a = x;
+	}
+	@Override
+	double f(double x) {
+		return a*a;
+	}
 	
 }
 
-class SinX{
+class SinX extends Hw01{
+	double a;
+	SinX(double x){
+		a = x;
+	}
+	@Override
+	double f(double x) {
+		return Math.sin(a);
+	}
 	
 }
 
-class CosX{
-	
+class CosX extends Hw01{
+	double a;
+	CosX(double x){
+		a = x;
+	}
+	@Override
+	double f(double x) {
+		return 0;
+	}
 }
 
-class Cos5Sin{
-	
+class Cos5Sin extends Hw01{
+	double a;
+	Cos5Sin(double x){
+		a = x;
+	}
+	@Override
+	double f(double x) {
+		return 0;
+	}
 }
 
-class Sin5Cos{
-	
+class Sin5Cos extends Hw01{
+	double a;
+	Sin5Cos(double x){
+		a = x;
+	}
+	@Override
+	double f(double x) {
+		return 0;
+	}
 }
 
-class Log{
-	
+class Log extends Hw01{
+	double a;
+	Log(double x){
+		a = x;
+	}
+	@Override
+	double f(double x) {
+		return 0;
+	}
 }
