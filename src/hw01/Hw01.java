aa9036163707890fadd4426459dcf79b97f68a57
chi-rui <xic768210@yahoo.com.tw>
2014-03-09 13:31:00 +0800

    先建立整體架構
    six class

---
+++ src/hw01/Hw01.java
@@ -0,0 +1,29 @@
+package hw01;
+
+public abstract class Hw01 {
+	abstract double f(double x); 	
+}
+
+class X2{
+	
+}
+
+class SinX{
+	
+}
+
+class CosX{
+	
+}
+
+class Cos5Sin{
+	
+}
+
+class Sin5Cos{
+	
+}
+
+class Log{
+	
+}
